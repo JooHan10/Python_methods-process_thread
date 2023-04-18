2ff37a1995f386b1e339a28c3d71b9bcf85ba679
I Joo Han <juhanlee2218@gmail.com>
2023-04-18 13:57:03 +0900

    edit/ edit process.py's content

--- process&thread/process.py
+++ process&thread/process.py
@@ -1,1 +1,55 @@
-import os
+#####################################
+# print('hello, os')
+#####################################
+
+#####################################
+# import os
+
+# print('hello, os')
+# print('ijh\'s pid is', os.getpid())
+#####################################
+
+#####################################
+# from multiprocessing import Process
+# import os
+
+# def ijh():
+#     print('child process', os.getpid())
+#     print('my parent is', os.getppid())
+
+# if __name__ == '__main__':
+#     print('parent process', os.getpid())
+#     child = Process(target=ijh).start()
+#####################################
+
+#####################################
+# from multiprocessing import Process
+# import os
+
+# def ijh():
+#     print('같은 작업 중')
+#     print('child process', os.getpid())
+
+# if __name__ == '__main__':
+#     print('parent process', os.getpid())
+#     child1 = Process(target=ijh).start()
+#     child2 = Process(target=ijh).start()
+#     child3 = Process(target=ijh).start()
+#####################################
+
+#####################################
+# from multiprocessing import Process
+# import os
+
+# def I():
+#     print('This is I')
+# def Joo():
+#     print('This is Joo')
+# def Han():
+#     print('This is Han')
+
+# if __name__ == '__main__':
+#     child1 = Process(target=I).start()
+#     child2 = Process(target=Joo).start()
+#     child3 = Process(target=Han).start()
+#####################################
